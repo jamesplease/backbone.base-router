5a7989142d4e5576057b57a346154b5be8a7eb33
Jmeas <jellyes2@gmail.com>
2014-10-05 21:00:51 -0400

    v0.3.0

--- dist/backbone.base-router.js
+++ dist/backbone.base-router.js
@@ -1,4 +1,4 @@
-// Backbone.BaseRouter v0.2.0
+// Backbone.BaseRouter v0.3.0
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['backbone', 'underscore'], function(Backbone, _) {
@@ -30,9 +30,6 @@
   
   Backbone.BaseRouter = Backbone.Router.extend({
   
-    // The instance of history that this Router uses
-    history: Backbone.history,
-  
     // The single point of entry. This is called whenever a
     // route is matched. The routeData argument contains lots of
     // useful information.
@@ -54,7 +51,7 @@
   
       // Register a callback with history
       var router = this;
-      this.history.route(route, function(fragment, navOptions) {
+      Backbone.history.route(route, function(fragment, navOptions) {
         var routeParams = router._extractParameters(route, fragment);
         var queryString = routeParams.pop();
   
